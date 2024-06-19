2fc56727d3fbe54b966377cce00e010b372ed85b
Fernando Bonfim <fernando.j.bonfim@gmail.com>
2024-06-19 19:28:43 -0300

    Initial files

---
+++ src/tipodevariaveldemo2/TipoDeVariavelDemo2.java
@@ -0,0 +1,18 @@
+package tipodevariaveldemo2;
+
+public class TipoDeVariavelDemo2 {
+
+    public static void main(String[] args) {
+
+        // Declaramos e iniciamos duas variáveis.
+        double altura = 3.2;
+        double base = 2.0;
+
+        // Cálcula a área do triângulo.
+        double area = (base * altura) / 2;
+
+        System.out.println("Área do triângulo: " + area + " metros quadrados.");
+
+    }
+    
+}
